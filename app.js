64ff60ec1266f7b71544fed5212b31c1adb4681e
nashEm8 <emanuel82022@outlook.com>
2022-12-17 21:07:47 -0300

    Criação da função que pega o valor do input ao ser digitado, request da chave api + o valor digitado e criação da imagem passando o src da url gerada e assim exibindo os gifs

---
+++ app.js
@@ -0,0 +1,74 @@
+/*
+  Neste exercício, vamos praticar um pouco do que vimos até aqui, através da 
+  API do GIPHY, que é um website de GIFs (https://giphy.com/).
+
+  - Seu desafio é implementar uma funcionalidade de busca dos GIFs. Quando o 
+    usuário digitar o termo de busca no input e pressionar enter, um GIF deve 
+    ser exibido na tela. Como neste exemplo: https://youtu.be/RHe-uCJGCeA
+  - Observe que o GIF mais recente é inserido acima dos GIFs anteriores;
+  - Para fazer requests para a API do GIPHY, você precisará de uma API key. 
+    Para obtê-la, siga os seguintes passos:
+    - Acesse https://developers.giphy.com/dashboard/ e faça o login;
+    - No Dashboard, clique em "Create an App", clique em "API Selected" e em 
+      "Next Step";
+    - Dê um nome e descrição para o app e crie-o;
+    - Clique no código da API key para copiá-la;
+  - O submit do form deve ser feito para o endpoint abaixo. Atente-se para os 2
+    [PLACEHOLDERS] que devem ser substituídos:
+    - https://api.giphy.com/v1/gifs/search?api_key=[SUA_CHAVE_DA_API_AQUI]&limit=1&q=[VALOR_INSERIDO_NO_INPUT]
+    - Se quiser testar outras possibilidades, os endpoints da API estão 
+      listados na documentação: https://developers.giphy.com/docs/api/endpoint#search
+  - Ignore os avisos no console. Para limpá-lo, pressione "ctrl + L".
+*/
+
+const form = document.querySelector('form');
+const showGif = document.querySelector('div');
+
+const api_key = "3OCShDUNie0iCsbgQuW1zn7oHzazqSyl";
+
+const getGifUrl = gifName =>
+ `https://api.giphy.com/v1/gifs/search?apikey=${api_key}&limit=1&q=${gifName}`;
+
+ const generateImg = (downsizedGifUrl, gifData) => {
+  const img = document.createElement('img')
+
+  img.setAttribute('src', downsizedGifUrl);
+  img.setAttribute('alt', gifData.data[0].title)
+
+  return img;
+}
+
+const fecthGif = async inputValue => {
+  try {
+    const gifUrl = getGifUrl(inputValue);
+    const response = await fetch(gifUrl)
+      
+      if(!response.ok){
+        throw new Error('Não foi possível obter os dados!');
+      }
+      return response.json();
+    } catch(error){
+    console.log(`Error: ${error.message}`);
+  }
+}
+
+const insertIntoDom = async inputValue => {
+      const gifData = await fecthGif(inputValue);
+
+      if(gifData){
+        const downsizedGifUrl = gifData.data[0].images.downsized.url;
+        const img = generateImg(downsizedGifUrl, gifData);
+        showGif.insertAdjacentElement('afterbegin', img);
+
+        form.reset();
+      }
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const inputValue = event.target.search.value;
+
+  insertIntoDom(inputValue);
+}); 
+
